d3f7547f48089ba6749bbde6a7a4c0dd1189fc6d
Nadzib <nadzib22@gmail.com>
2024-02-09 23:27:13 +0700

    update

--- src/fumction.kt
+++ src/fumction.kt
@@ -1,17 +1,28 @@
-val buah:List<String> = listOf("Apel", "Semongko", "Sersat")
+val buah:MutableList<String> = mutableListOf("Apel", "Semongko", "Sersat")
 fun main() {
     penjumlahan(90,44)
     println(pengurangan(12,16))
     nama("Nadzib hibatulloh", 50)
-
+//  Umur
     var umur:Int
     umur = 19
     println(umur)
-
+//  Nama
     val nama = "Nadzib"
     println(nama)
-
+//  banyaknya buah
     println("banyaknya buah yang diinputkan hari ini adalah ${buah.size}")
+//  print setiap buah
+    buah.forEach{
+        i -> println("Ini adalah $i")
+    }
+//  menambahkan buah
+    buah.add("anggur")
+    println("banyak buah yang baru ${buah.size}")
+    buah.forEach{
+        i -> println("Ini adalah $i")
+    }
+//
 }
 
 fun penjumlahan(x:Int, y:Int){
